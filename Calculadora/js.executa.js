c27b8d98dbb0912ec6fcbd1ac71202f52f9aae04
paulolima12 <hrpaulo1204@gmail.com>
2024-02-29 11:09:27 -0300

    preparação calculadora

---
+++ Calculadora/js.executa.js
@@ -0,0 +1,7 @@
+var resultado = document.getElementById('resultado')
+
+function pegaNOME(){
+    var nome = document.getElementById('nome').value
+    //console.log('Nome:',nome)
+    resultado.innerHTML = nome
+}
