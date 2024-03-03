0b4a0b6604a211332bbfee8ab48ffd5d11596c33
Lima <124838287+paulolima12@users.noreply.github.com>
2024-03-03 19:42:29 -0300

    Delete Calculadora directory

--- Calculadora/js.executa.js
+++
@@ -1,7 +0,0 @@
-var resultado = document.getElementById('resultado')
-
-function pegaNOME(){
-    var nome = document.getElementById('nome').value
-    //console.log('Nome:',nome)
-    resultado.innerHTML = nome
-}
